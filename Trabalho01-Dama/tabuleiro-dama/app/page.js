0ea55f2d6bf69c957cda68abf78e577f39edc1ac
Lucas Antunes <cleiton.lucas416@gmail.com>
2025-02-20 10:16:34 -0300

    Exibindo o tabuleiro centralizado

--- Trabalho01-Dama/tabuleiro-dama/app/page.js
+++ Trabalho01-Dama/tabuleiro-dama/app/page.js
@@ -1,7 +1,11 @@
-export default function Page(){
-    return(
-        <div>
-            <h1>Tabuleiro de Dama</h1>
+import Tabuleiro from './components/Tabuleiro';
+
+export default function Home() {
+    return (
+        // Centralizando o tabuleiro e exibindo o título
+        <div style={{ textAlign: 'center', padding: '20px', backgroundColor: '#333', height: '100vh' }}>
+            <h1 style={{ color: 'white' }}>Tabuleiro de Dama</h1>
+            <Tabuleiro />
         </div>
-    )
+    );
 }
